278d33fd29af310bbdeae5b4c6943584a36d4ae6
Sithum-Ravishka <79629419+Sithum-Ravishka@users.noreply.github.com>
2022-04-25 02:05:28 +0530

    powerconsu Insert part complete

--- ElectroGrid/src/com/PowerConsumptionService.java
+++ ElectroGrid/src/com/PowerConsumptionService.java
@@ -1,5 +1,51 @@
 package com;
 
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+import model.PowerConsumption;
+
+
+
+@Path("/powerconsumption")
+
 public class PowerConsumptionService {
 
+	PowerConsumption consu = new PowerConsumption();
+	
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML) 
+	
+	public String readPowerConsumption() 
+	{ 
+		return consu.readPowerConsumption(); 
+	
+	}
+	
+	@POST
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+
+	public String insertPowerConsumption(@FormParam("c_ID") String c_ID, 
+						@FormParam("c_commercial") String c_commercial, 
+						@FormParam("c_agriculture") String c_agriculture, 
+						@FormParam("c_residential") String c_residential,
+						@FormParam("c_date") String c_date )
+	
+	{ 
+	
+		String output = consu.insertPowerConsumption( c_ID, c_commercial, c_agriculture, c_residential, c_date ); 
+		return output; 
+
+	}
 }
---
+++ ElectroGrid/src/model/PowerConsumption.java
@@ -0,0 +1,125 @@
+package model;
+
+import java.sql.*; 
+
+public class PowerConsumption {
+
+		//A common method to connect to the DB
+		private Connection connect()
+		{
+			//Create database connection
+			 String dbURL = "jdbc:mysql://localhost:3306/power-consumption";
+		     String dbUser = "root";
+		     String dbPass = "199808";
+		     
+			Connection con = null;
+			try
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+				//Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection(dbURL, dbUser, dbPass);
+			}
+			catch (Exception e)
+			{e.printStackTrace();}
+			return con;
+		}
+		
+		public String insertPowerConsumption( String c_ID, String c_commercial, String c_agriculture, String c_residential, String c_date ) { 
+			
+			String output = ""; 
+		 
+			try { 
+				Connection con = connect();
+		  
+				if (con == null) {
+					return "Error while connecting to the database for inserting."; 
+				} 
+		 
+				String query = " insert into PowerConsumption (`c_ID`, `c_commercial`,`c_agriculture`,`c_residential`, `c_date`)" + " values (?, ?, ?, ?, ?)"; 
+				PreparedStatement preparedStmt = con.prepareStatement(query); 
+		 
+				
+				preparedStmt.setString(1, c_ID); 
+				preparedStmt.setString(2, c_commercial); 
+				preparedStmt.setString(3, c_agriculture); 
+				preparedStmt.setString(4, c_residential); 
+				preparedStmt.setString(5, c_date);
+		 
+		
+				preparedStmt.execute(); 
+				con.close(); 
+		 
+				output = "Inserted successfully"; 
+			} 
+			catch (Exception e) 
+			{ 
+				output = "Error while inserting the PowerConsumption."; 
+				System.err.println(e.getMessage()); 
+			} 
+			return output; 
+		 } 
+		
+		
+	public String readPowerConsumption() 
+		 { 
+			String output = ""; 
+			
+			try
+			{ 
+				Connection con = connect(); 
+				if (con == null) {
+					return "Error while connecting to the database for reading.";
+					} 
+		
+				output = "<table border='1'> <tr><th>Pay ID</th>"
+						+ "<th>Holder Name</th>"
+						+ "<th>Card Type</th>"
+						+ "<th>Card No</th>"
+						+ "<th>CVV</th></tr>";
+; 
+		 
+				String query = "select * from PowerConsumption"; 
+				Statement stmt = con.createStatement(); 
+				ResultSet rs = stmt.executeQuery(query); 
+		 
+		 
+		
+				while (rs.next()) 
+				{ 
+			 
+					String c_ID = rs.getString ("c_ID"); 
+					String c_commercial = rs.getString("c_commercial"); 
+					String c_agriculture = rs.getString("c_agriculture"); 
+					String c_residential = rs.getString("c_residential"); 
+					String c_date = rs.getString("c_date"); 
+		
+					output += "<tr><td>" + c_ID + "</td>";
+					output += "<td>" + c_commercial + "</td>";
+					output += "<td>" + c_agriculture + "</td>"; 
+					output += "<td>" + c_residential + "</td>"; 
+					output += "<td>" + c_date + "</td>"; 
+		 
+					 // buttons 		
+					   output
+							  += "<td><input name='btnUpdate' "
+							  + " type='button' value='Update' class='btn btn-secondary' </td>"
+					 		  + "<td><form method='post' action='Products.jsp'>"
+					 		  + "<input name='btnRemove' " + " type='submit' value='Remove' class='btn btn-danger'>"
+					 		  + "<input name='PowerConsumption' type='hidden' " + " value='" + c_ID + "'>" + "</form></td></tr>";
+					 		 
+				} 
+				
+		 con.close(); 
+		 
+		// Complete the html table
+		 output += "</table>"; 
+		 } 
+		 catch (Exception e) 
+		 { 
+			 output = "Error while reading the PowerConsumption."; 
+			 System.err.println(e.getMessage()); 
+		 } 
+		 return output; 
+		 } 
+		
+}
